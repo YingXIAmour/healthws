af0085684a58fe15d62d15eb3d60ce14809a5727
Searching-Dream <dream11451415926@gmail.com>
2026-06-19 00:44:15 +0800

    feat(client): 添加玩家状态效果数据收集功能
    - 集成 Gson 库支持 JSON 数组操作
    - 添加对玩家状态效果列表的遍历和序列化
    - 新增药水效果数组到健康数据 JSON 结构
    - 重构伤害源获取逻辑并优化实体类型检查
    - 移除冗余的伤害源名称和数量字段
    - 更新伤害类型获取方式以符合新版本 API

--- src/main/java/com/yingxiya/healthws/client/HealthwsClient.java
+++ src/main/java/com/yingxiya/healthws/client/HealthwsClient.java
@@ -1,17 +1,23 @@
 package com.yingxiya.healthws.client;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.entry.RegistryEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Collection;
 
 public class HealthwsClient implements ClientModInitializer {
     private static final Logger LOG = LoggerFactory.getLogger("healthws");
@@ -63,24 +69,35 @@ public class HealthwsClient implements ClientModInitializer {
                 json.addProperty("Damage", realDamage);
                 json.addProperty("isDead", isDead);
 
+                Collection<StatusEffectInstance> effects = player.getStatusEffects();
+                if (!effects.isEmpty()) {
+                    JsonArray potionArray = new JsonArray();
+                    for (StatusEffectInstance effect : effects) {
+                        JsonObject potionObj = new JsonObject();
+                        RegistryEntry<StatusEffect> effectEntry = (RegistryEntry<StatusEffect>) effect.getEffectType();
+                        String effectName = effectEntry.getKeyOrValue().toString();
+                        potionObj.addProperty("name", effectName);
+                        potionObj.addProperty("amplifier", effect.getAmplifier());
+                        potionObj.addProperty("duration", effect.getDuration());
+                        potionObj.addProperty("isAmbient", effect.isAmbient());
+                        potionObj.addProperty("visible", effect.shouldShowParticles());
+                        potionArray.add(potionObj);
+                    }
+                    json.add("potions", potionArray);
+                }
+
                 if (pendingDamageSource != null && realDamage > 0) {
-                    Entity attacker = pendingDamageSource.getAttacker();
-                    if (attacker != null) {
-                        json.addProperty("attackerName", attacker.getName().getString());
+                    Entity attacker = pendingDamageSource.getSource();
+                    if (attacker instanceof LivingEntity) {
+                        LivingEntity livingAttacker = (LivingEntity) attacker;
+                        json.addProperty("attackerName", livingAttacker.getName().getString());
                         json.addProperty("attackerType", attacker.getType().toString());
 
                         double distance = attacker.squaredDistanceTo(player);
                         json.addProperty("distance", Math.round(distance * 100.0) / 100.0);
                     }
 
-                    Entity sourceEntity = pendingDamageSource.getSource();
-                    if (sourceEntity != null && sourceEntity != attacker) {
-                        json.addProperty("sourceName", sourceEntity.getName().getString());
-                        json.addProperty("sourceType", sourceEntity.getType().toString());
-                    }
-
-                    json.addProperty("damageTypeName", pendingDamageSource.getName());
-                    json.addProperty("damageAmount", pendingDamageAmount);
+                    json.addProperty("damageTypeName", pendingDamageSource.getType().msgId());
 
                     pendingDamageSource = null;
                     pendingDamageAmount = 0.0F;
